59b45563ffe23690d0f0805e62f9292e7f038379
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-03-22 20:19:46 +0330

    :beers:

--- Online_shop/product/urls.py
+++ Online_shop/product/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import view_product, product_detail
+from .views import view_product, product_detail, add_to_cart
 
 
 app_name = "product"
 urlpatterns = [
     path("", view_product, name="products"),
     path("product_detail/<int:prod_id>", product_detail, name="product_detail"),
+    path("add-to-cart/", add_to_cart, name="add-to-cart"),
 ]
